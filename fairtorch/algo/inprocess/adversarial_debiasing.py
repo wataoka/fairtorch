62fcd9def909fde759923fe41d22f7ca024df7a2
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-12-07 20:45:42 +0900

    add: adversarial_debiasing

---
+++ fairtorch/algo/inprocess/adversarial_debiasing.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+class Classifier(nn.Module):
+
+    def __init__(self, n_features, n_hidden=32, p_dropout=0.2):
+        super(Classifier, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(n_features, n_hidden),
+            nn.ReLU(),
+            nn.Dropout(p_dropout),
+            nn.Linear(n_hidden, n_hidden),
+            nn.ReLU(),
+            nn.Dropout(p_dropout),
+            nn.Linear(n_hidden, n_hidden),
+            nn.ReLU(),
+            nn.Dropout(p_dropout),
+            nn.Linear(n_hidden, 1),
+        )
+    
+    def forward(self, x):
+        return torch.sigmoid(self.network(x))
+
+class Adversary(nn.Module):
+
+    def __init__(self, n_sensitive, n_hidden=32):
+        super(Adversary, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(1, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, n_hidden),
+            nn.ReLU(),
+            nn.Linear(n_hidden, n_sensitive),
+        )
+    
+    def forward(self, x):
+        return torch.sigmoid(self.network(x))
