a0e3da9187bbca43cee4b7228ca9994bf4ddc354
Koki Wataoka <wataoka@stu.kobe-u.ac.jp>
2019-12-07 20:45:17 +0900

    add: sex attribute in german.py

--- fairtorch/datasets/german.py
+++ fairtorch/datasets/german.py
@@ -1,30 +1,45 @@
 import os
 import sys
 
+import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
 
+THIS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.join(THIS_FILE_DIR, '..')
+GERMAN_SEX_PATH = os.path.join(BASE_DIR, 'data', 'german', 'sex.npy')
+
 class German(Dataset):
 
-    def __init__(self, root, transform=None):
-        self.features = ['status_of_credit', 'duration_in_month', 'credit_history',
+    def __init__(self, data_numeric_path, transform=None, protected_attribute_names=['sex'], sensitive_attribute_path=GERMAN_SEX_PATH, preprocess='min-max'):
+        self.transform = transform
+        self.data_names = ['status_of_credit', 'duration_in_month', 'credit_history',
                          'credit_amout', 'savings', 'job', 'feature7', 'housing',
                          'property', 'age', 'plans', 'feature12', 'feature13',
                          'feature14', 'feature15', 'feature16', 'feature17',
                          'feature18', 'feature19', 'feature20', 'feature21',
                          'feature22', 'feature23', 'feature24']
-        self.labels = ['label']
-        self.column_names = self.features + self.labels
-        self.df = pd.read_csv(
-            root,
-            delimiter='\s+',
-            names=(self.features + self.labels)
-        )
-        self.transform = transform
+        self.label_names = ['label']
+        self.df = pd.read_csv(data_numeric_path, delimiter='\s+', names=(self.data_names + self.label_names))
+        self.data = self.df[self.data_names].values
+        self.label = self.df[self.label_names].values
+        self.sensitive_attribute = np.array(np.load(sensitive_attribute_path))
+        if preprocess=='min-max':
+            self.data = self.min_max(self.data, axis=1)
+            self.label = self.min_max(self.label, axis=0)
+            self.sensitive_attribute = self.min_max(self.sensitive_attribute, axis=0)
     
     def __len__(self):
         return len(self.df)
     
     def __getitem__(self, idx):
-        labels = self.df.iat(idx, self.labels)
-        features = self.df.loc(idx, self.features)
+        out_data = self.data[idx]
+        out_label = self.label[idx]
+        out_sensitive_attribute = self.sensitive_attribute[idx]
+
+        return out_data, out_label, out_sensitive_attribute
+    
+    def min_max(self, x, axis=None):
+        x_min = x.min(axis=axis, keepdims=True)
+        x_max = x.max(axis=axis, keepdims=True)
+        return (x-x_min)/(x_max-x_min)
